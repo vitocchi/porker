5b31ab3d193afe6924c7c3fc7ad6f59fc3b303af
Kosuke Ito <ito-kosuke0502@g.ecc.u-tokyo.ac.jp>
2019-04-24 16:19:43 +0900

    libにモジュールを実装

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,9 @@
-// トランプのカード
-#[derive(Debug)]
-struct Card {
-    suit: Suit, // マーク
-    number: u8, //数字
-}
+extern crate porkerlib;
 
-// トランプのマーク
-#[derive(Debug)]
-enum Suit {
-    Spade,
-    Diamond,
-    Club,
-    Heart
-}
-
-impl Card {
-    fn new(suit: Suit, number:u8) -> Result<Card, String> {
-        Card::check_number(number)?;
-        Ok(Card{suit, number})
-    }
-
-    fn check_number(number:u8) -> Result<(), String> {
-        if number > 13 || number == 0 {
-            return Err(number.to_string() + " is to large as number of card!!");
-        }
-        return Ok(())
-    }
-}
-
-
+use porkerlib::Card;
+use porkerlib::Suit;
 fn main() {
     println!("Hello, world!");
-    let card = Card::new(Suit::Club, 0).unwrap();
+    let card = Card::new(Suit::Club, 2).unwrap();
     println!("{:?}", card);
 }
