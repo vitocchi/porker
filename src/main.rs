5b0f4593efbcaef0656221150d67d12393c00b09
Kosuke Ito <ito-kosuke0502@g.ecc.u-tokyo.ac.jp>
2019-04-25 17:34:27 +0900

    コメント

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@ extern crate porkerlib;
 
 use porkerlib::table;
 fn main() {
+    // ゲームを初期化する (デッキに整列されたカード52枚、手札0枚)
     let mut table = table::Table::new();
+
+    // デッキをシャッフルする
     table.shuffle_deck();
-    println!("{}", table);
+
+    // カードを5枚引く
     table.pick_from_deck().unwrap();
     table.pick_from_deck().unwrap();
     table.pick_from_deck().unwrap();
     table.pick_from_deck().unwrap();
     table.pick_from_deck().unwrap();
+
+    // 出力
     println!("{}", table);
 }
